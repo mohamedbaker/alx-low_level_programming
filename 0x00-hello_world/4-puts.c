32c09690a3dcdb26ae017c858b7ce8c4bdff2d4f
mohamedbaker <dam.vampire22@gmail.com>
2023-08-10 19:52:18 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int  main (){
-puts ("Programming is like building a multilingual puzzle\n");
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that writes strings to stdout using puts function
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
