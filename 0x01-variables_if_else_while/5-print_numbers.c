fa1e8d58ee20b414bc43f3f488383faca640d724
mohamedbaker <dam.vampire22@gmail.com>
2023-08-13 08:34:24 -0700

    print only 1 digit nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Contains whole code
+*Description: print only 1 digit nums
+*Return: 0 for (successfull)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num <= 11; num++)
+	{
+		if (num / 10 == 0)
+		{
+		printf("%d", num);
+		}
+	}
+		puts("\n");
+		return (0);
+}
