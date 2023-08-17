005d8a8fe7803c8c66f2b06c1e025fb7dffc334f
mohamedbaker <dam.vampire22@gmail.com>
2023-08-17 15:23:06 -0700

    asd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_abs - Enery Point
+*Description: abs values
+*@num: ARG
+*Return: 0 for successfull
+*/
+
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (-num);
+}
