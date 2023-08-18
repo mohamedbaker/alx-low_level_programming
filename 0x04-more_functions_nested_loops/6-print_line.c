202c167cb4d7aeddb36e11e3f2c51f135886ec7e
mohamedbaker <dam.vampire22@gmail.com>
2023-08-17 20:07:22 -0700

    s

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_line - draws line in the terminal
+*@n: num
+*return: -
+*/
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
