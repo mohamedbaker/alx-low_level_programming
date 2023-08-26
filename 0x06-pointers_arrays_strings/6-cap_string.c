e606cb23430891549e2cb449f72d7127f7b6b115
mohamedbaker <dam.vampire22@gmail.com>
2023-08-25 19:47:49 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes everey char of a string
- * @s: string to Cap
- * Return: the result
+ * isDelim - found Delim
+ * @c: string to Cap
+ * Return: 1 if is Delim
 */
 
 int isDelim(char c)
 {
 	int i;
-	char delim [] = {' ', '\t', '\n', ',', ';', '.',
+	char delim[] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; i < 12; i++)
-		if ( c == delim[i])
+		if (c == delim[i])
 			return (1);
 	return (0);
 }
+
+/**
+ * cap_string - capitalizes everey char of a string
+ * @s: string to Cap
+ * Return: the result
+*/
 
 char *cap_string(char *s)
 {
