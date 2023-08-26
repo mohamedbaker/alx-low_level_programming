3679b9ec4c7803a0eee949b1401034d7d0b41855
mohamedbaker <dam.vampire22@gmail.com>
2023-08-25 21:50:47 -0700

    a

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - change strings 
+ * @s: string
+ * Return: the result
+*/
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] < sizeof(a) ; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
