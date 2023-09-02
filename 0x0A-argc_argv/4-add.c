12456422e3bf309a496f90a264c62b91d14be080
mohamedbaker <dam.vampire22@gmail.com>
2023-09-01 19:59:45 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-			if(*c < '0' || *c > '9')
-				return (printf("Error\n"),1);
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
