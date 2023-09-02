f8f252528f444580e27a1ffd0cb68244933fd77f
mohamedbaker <dam.vampire22@gmail.com>
2023-09-01 19:57:28 -0700

    a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ * Return: 0 success , 1 error
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if(*c < '0' || *c > '9')
+				return (printf("Error\n"),1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
