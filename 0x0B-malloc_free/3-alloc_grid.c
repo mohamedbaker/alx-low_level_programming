2d9bc3f458e70b5239af1ec5db0946ccadeab0ee
mohamedbaker <dam.vampire22@gmail.com>
2023-09-05 19:52:52 -0700

    aa

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,20 +7,28 @@
  * @height: height of the matrix
  * Return: pointer to matrix (Success)
  */
+
 int **alloc_grid(int width, int height)
-	{
+{
 	int **pg, i, j;
 
 	pg = malloc(sizeof(pg) * height);
 
-		if (width <= 0 || height <= 0 || tab == 0)
+		if (width <= 0 || height <= 0 || pg == 0)
 		{
-			pg[i] = malloc(sizeof(**pg) * width);
-			if (pg[i] == 0)
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < height; i++)
 			{
-				while (i--)
-					free(pg[i]);
-				free(pg);
+
+				pg[i] = malloc(sizeof(**pg) * width);
+				if (pg[i] == 0)
+				{
+					while (i--)
+						free(pg[i]);
+					free(pg);
 				return (NULL);
 			}
 			for (j = 0; j < width; j++)
