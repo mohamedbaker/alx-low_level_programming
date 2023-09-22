20a1f7a458f7cce43dd2e447056ad70350ea9ba1
mohamedbaker <dam.vampire22@gmail.com>
2023-09-21 19:46:08 -0700

    aa

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements
+ * @h: pointer to list
+ * Return: elements count
+*/
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
