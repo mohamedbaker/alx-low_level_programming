1e54a03c37a3b0fd5c38a83a4711d8221191970f
mohamedbaker <dam.vampire22@gmail.com>
2023-09-26 19:54:10 -0700

    temp

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node by indx
+ * @head: head node
+ * @index: index
+ * Return: 1 if it succeeded, -1 if it failed
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert new node
+ * @head: node head
+ * @idx: index
+ * @n: node data
+ * Return: new node address
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+}
