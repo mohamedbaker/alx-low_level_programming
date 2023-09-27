35388efd5de55f0eaca5cee055938b2700838a11
mohamedbaker <dam.vampire22@gmail.com>
2023-09-26 19:36:18 -0700

    a

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Delete head
+ * @head: head node
+ * Return: data 
+**/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int node_data;
+
+	if (!*head)
+		return (0);
+
+	temp = *head;
+	*head = temp->next;
+	node_data = temp->n;
+	free(temp);
+	return (node_data);
+}
