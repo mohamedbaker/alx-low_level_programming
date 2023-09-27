8d7cb09344bcc1edc4c5b7015415dae345f58f9d
mohamedbaker <dam.vampire22@gmail.com>
2023-09-26 18:45:01 -0700

    null fix

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - the number of elems
+ * @h: head node
+ * Return: nodes count
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while(h)
+	{
+	h = h->next;
+	counter++;
+	}
+	return (counter);
+}
