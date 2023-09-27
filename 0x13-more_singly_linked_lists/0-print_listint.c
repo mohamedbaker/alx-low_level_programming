907cf847a26a5d72ade6887ccd6a0884a16cd8af
mohamedbaker <dam.vampire22@gmail.com>
2023-09-26 18:35:58 -0700

    first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all nodes
+ * @h: head of linked list
+ * Return: nodes count
+ *
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (count);
+}
