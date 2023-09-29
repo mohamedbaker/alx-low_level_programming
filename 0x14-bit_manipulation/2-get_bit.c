077d44b948928a357f2f2e6f78710b1b6849aa5c
mohamedbaker <dam.vampire22@gmail.com>
2023-09-28 19:39:23 -0700

    aa

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - value of a bit at index
+ * @n: num
+ * @index: index
+ * Return: bit at index
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int shift = n >> index;
+
+	return (shift & 1);
+}
