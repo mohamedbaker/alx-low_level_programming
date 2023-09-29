5064fcfb00429470ff46100c963e7cf023825791
mohamedbaker <dam.vampire22@gmail.com>
2023-09-28 19:24:31 -0700

    a

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary represent
+ * @n: num to convert
+*/
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar(n & 1);
+ 
+}
