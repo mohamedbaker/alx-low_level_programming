ac2646ef716d7a927c0c03dd044dc1e9cbfc486c
mohamedbaker <dam.vampire22@gmail.com>
2023-10-02 18:31:47 -0700

    Read and  print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it.
+ * @filename: name of the fil to read.
+ * @letters: number of letters.
+ * Return: numbers of letters or 0 in fail.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char *buffer;
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	bytes = read(fd, buf, letters);
+	bytes = write(STDOUT_FILENO, buf, bytes);
+
+	close(fd);
+
+	free(buffer);
+
+	return (bytes);
+}
