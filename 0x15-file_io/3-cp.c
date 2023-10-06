31a70e278cb080845f85314be6d1c89f4ece499d
mohamedbaker <dam.vampire22@gmail.com>
2023-10-06 00:34:49 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#define ERROR "Error: Can't read from file %s\n"
 /**
  * main - copy content from a file to another file
  * @argc: num arg
@@ -14,17 +15,15 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	source = open(argv[1], O_RDONLY);
-	if (source< 0)
+	if (source < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, ERROR, argv[1]), exit(98);
 	}
 	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((fd= read(source, buf, READ_BUF_SIZE)) > 0)
+	while ((fd = read(source, buf, READ_BUF_SIZE)) > 0)
 	{
 		if (dest < 0 || write(dest, buf, fd) != fd)
 		{
@@ -33,24 +32,20 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (fd< 0)
+	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, ERROR, argv[1]), exit(98);
 	}
-
 	error_close = close(source);
 	if (error_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source), exit(100);
 	}
 
 	error_close = close(dest);
 	if (error_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
 	}
 	return (0);
 }
