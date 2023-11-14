dfd5b005c8714f3ed38eeb51365dad264413d1f5
mohamedbaker <dam.vampire22@gmail.com>
2023-11-14 04:29:14 +0200

    a

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+int mul(int x, int y)
+{
+	 return (x * y);
+}
+
+int div(int x, int y)
+{
+	if (y == 0) {
+		return (0);
+	}
+	return x / y;
+}
+
+int mod(int x, int y)
+{
+	if (y == 0) {
+		return (0);
+	}
+	return (x % y);
+}
